daa550bf35db178dc92b4ecf178185bec71fa6a7
aloladze88-afk <aloladze88@gmail.com>
2026-02-23 19:29:16 +0100

    Added 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0-9 using putchar without char variables
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
