33d9f7fb0b5572ca7c9167d958f11568ab224084
aloladze88-afk <aloladze88@gmail.com>
2026-02-24 19:41:05 +0100

    Added 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int row, col, prod;
+
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+
+		for (col = 1; col <= 9; col++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = row * col;
+
+			if (prod < 10)
+			{
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
