4ca176a17d2432301659ae08959a135d6de31d31
aloladze88-afk <aloladze88@gmail.com>
2026-03-05 13:29:39 +0100

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: pointer to an array of 8 chars (each row)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
+}
