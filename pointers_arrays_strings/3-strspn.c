18f16b2f0e0906ba73a9ec3df03d31406aa13147
aloladze88-afk <aloladze88@gmail.com>
2026-03-05 13:39:58 +0100

    Added 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to scan
+ * @accept: string containing the accepted bytes
+ *
+ * Return: number of bytes in the initial segment of s consisting
+ *         only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+
+		if (!found)
+			break;
+	}
+
+	return (i);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,6 +24,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 #endif
