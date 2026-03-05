ac790e2e2a180f7118b9a42fc835f659f0043f6e
aloladze88-afk <aloladze88@gmail.com>
2026-03-05 10:16:48 +0100

    fix betty

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,14 +10,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    return (dest);
+	return (dest);
 }
