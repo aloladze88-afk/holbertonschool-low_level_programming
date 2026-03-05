a636bf376b087da926d675172ed7edd1028fa59a
aloladze88-afk <aloladze88@gmail.com>
2026-03-05 13:26:52 +0100

    5-strstr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
 #endif
