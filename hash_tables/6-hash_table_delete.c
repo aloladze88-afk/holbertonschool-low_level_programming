7ca134045ec1fcc0afd56778497609d884ca083d
aloladze88-afk <aloladze88@gmail.com>
2026-03-31 08:38:50 +0200

    Add 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
