7428c708155e299ab1758a71d15e9fcc4a7c251a
aloladze88-afk <aloladze88@gmail.com>
2026-03-31 07:48:19 +0200

    fix lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -8,6 +8,8 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -16,6 +18,6 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
