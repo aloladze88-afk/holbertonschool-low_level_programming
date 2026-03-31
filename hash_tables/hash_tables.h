e11cfa7d5e4ab7aec682f71e0602022e86294081
aloladze88-afk <aloladze88@gmail.com>
2026-03-31 08:35:08 +0200

    Add 5

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,11 +1,11 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stddef.h>
-
 /**
  * struct hash_node_s - Node of a hash table
+ *
  * @key: The key, string
+ * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the list
  */
@@ -18,8 +18,10 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list
  */
 typedef struct hash_table_s
 {
@@ -32,5 +34,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 #endif
