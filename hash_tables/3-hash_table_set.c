a7fdbd39c46a146aa8518b857e25711299fd9cac
aloladze88-afk <aloladze88@gmail.com>
2026-03-31 08:30:28 +0200

    Add 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,89 @@
+#include "hash_tables.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string, or NULL if it fails
+ */
+char *_strdup(const char *str)
+{
+	char *copy;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+
+/**
+ * hash_table_set - adds or updates an element in a hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *current;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			new_value = _strdup(value);
+			if (new_value == NULL)
+				return (0);
+
+			free(current->value);
+			current->value = new_value;
+			return (1);
+		}
+		current = current->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = _strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->value = _strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,12 +2,14 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: The key
- * @value: The value associated with the key
- * @next: Pointer to the next node
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node
  */
 typedef struct hash_node_s
 {
@@ -17,9 +19,9 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- * struct hash_table_s - Hash table structure
- * @size: Size of the array
- * @array: Array of linked lists
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: An array of linked lists
  */
 typedef struct hash_table_s
 {
@@ -29,5 +31,7 @@ typedef struct hash_table_s
 
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
