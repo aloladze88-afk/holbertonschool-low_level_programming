29614d6469c107ff8c81c990fa55f38aa746bf03
aloladze88-afk <aloladze88@gmail.com>
2026-03-31 07:54:24 +0200

    Fix lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -16,8 +16,6 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif
