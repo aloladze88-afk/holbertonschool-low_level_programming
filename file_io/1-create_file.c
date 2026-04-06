2069e5006d75237de18c6f846b7e5ef869103228
aloladze88-afk <aloladze88@gmail.com>
2026-04-06 09:08:19 +0200

    Add 1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes text content into it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	len = 0;
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+
+		written = write(fd, text_content, len);
+		if (written == -1 || written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
